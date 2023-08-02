dfd707801439c169624a202f9c16707b7084700f
André Luiz Barros <77760155+andre55255@users.noreply.github.com>
2023-08-02 11:53:30 -0300

    Feat - Implemented find mime types from file static

--- Portfolio.Infrastructure/ServicesImpl/HandleFileService.cs
+++ Portfolio.Infrastructure/ServicesImpl/HandleFileService.cs
@@ -15,7 +15,7 @@ namespace Portfolio.Infrastructure.ServicesImpl
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ILogService _logService;
 
-        public HandleFileService(IFileUniqueService fileUniqueService, ILogService logService, IHttpContextAccessor httpContextAccessor)
+        public HandleFileService(ILogService logService, IHttpContextAccessor httpContextAccessor)
         {
             _fileUniqueService = new FileUniqueService(GetConfigsFileService());
             _logService = logService;
@@ -113,34 +113,14 @@ namespace Portfolio.Infrastructure.ServicesImpl
         {
             try
             {
+                List<Base64Data> list = GetAllBase64FromReadFileTxt();
+
                 FileConfigModel config = new FileConfigModel
                 {
                     BaseDirectory = Path.Combine(Path.GetFullPath("Directory")),
                     PathAvailableStaticFile = "/Static/",
                     BaseUrlApi = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}",
-                    Base64Data = new List<Base64Data>
-                    {
-                        new Base64Data
-                        {
-                            Extension = "xls,xlsx",
-                            MimeType = "data:application/vnd.ms-excel;base64"
-                        },
-                        new Base64Data
-                        {
-                            Extension = "png",
-                            MimeType = "data:image/png;base64"
-                        },
-                        new Base64Data
-                        {
-                            Extension = "jpg",
-                            MimeType = "data:image/jpg;base64"
-                        },
-                        new Base64Data
-                        {
-                            Extension = "jpeg",
-                            MimeType = "data:image/jpeg;base64"
-                        }
-                    }
+                    Base64Data = list
                 };
                 return config;
             }
@@ -153,6 +133,51 @@ namespace Portfolio.Infrastructure.ServicesImpl
             {
                 _logService.Write($"Falha inesperada ao criar arquivo de configuração de serviço de arquivo", this.GetPlace(), ex);
                 throw new ValidException($"Falha inesperada ao criar arquivo de configuração de serviço de arquivo");
+            }
+        }
+
+        private List<Base64Data> GetAllBase64FromReadFileTxt()
+        {
+            try
+            {
+                string filepath = Path.Combine(Path.GetFullPath("Content"), "Configs", "MimeTypesBase64HandleFileService.txt");
+                if (!File.Exists(filepath))
+                    throw new ConfigFileException($"Não foi encontrado o arquivo {filepath} para leitura, verifique");
+                
+                List<Base64Data> response = new List<Base64Data>();
+                using (FileStream fs = File.Open(filepath, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string readLine = sr.ReadLine();
+                            if (string.IsNullOrEmpty(readLine))
+                                continue;
+
+                            string[] register = readLine.Split("|");
+                            if (register == null || register.Length <= 1)
+                                continue;
+
+                            response.Add(new Base64Data
+                            {
+                                Extension = register[0],
+                                MimeType = register[1]
+                            });
+                        }
+                    }
+                }
+                return response;
+            }
+            catch (ConfigFileException ex)
+            {
+                _logService.Write(ex.Message, this.GetPlace(), ex);
+                throw new ValidException(ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                _logService.Write($"Falha inesperada ao ler lista de mime types do arquivo de configuração na api", this.GetPlace(), ex);
+                throw new ValidException($"Falha inesperada ao ler lista de mime types do arquivo de configuração na api");
             }
         }
     }
