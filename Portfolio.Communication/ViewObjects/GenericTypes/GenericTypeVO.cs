06dcb5359f390a6bea38aee0e854c21d3d890b35
André Luiz Barros <77760155+andre55255@users.noreply.github.com>
2023-08-10 13:13:58 -0300

    Fix - Default value generic type

--- Portfolio.Communication/ViewObjects/GenericTypes/GenericTypeVO.cs
+++ Portfolio.Communication/ViewObjects/GenericTypes/GenericTypeVO.cs
@@ -14,7 +14,7 @@ namespace Portfolio.Communication.ViewObjects.GenericTypes
         [StringLength(1024, ErrorMessage = "Descrição deve ter entre 1 e 1024 caracteres", MinimumLength = 1)]
         public string Description { get; set; }
         [Required(ErrorMessage = "Valor booleano não informado")]
-        public bool? ValueBool { get; set; }
+        public bool? ValueBool { get; set; } = false;
     }
 
     public class GenericTypeVO : SaveGenericTypeVO
